a2ab352e90363f5db81926248a2e9f4aea68837d
shahrukh-butt <shahrukh.butt@salsoft.net>
2025-05-14 17:56:14 +0500

    update cart pages

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -24,11 +24,11 @@ export default function CheckOut() {
 
 
     return (
-        <div className='flex flex-col items-center justify-center gap-10'>
+        <div className='flex flex-col items-center justify-center gap-10 px-3 md:px-0'>
 
             <p className='Bold-heading'>CHECKOUT</p>
 
-            <div className='rounded-box relative w-7xl  flex justify-center items-center cursor-pointer '>
+            <div className='rounded-box relative w-full lg:w-6xl  flex justify-center items-center cursor-pointer '>
 
 
 
@@ -95,37 +95,43 @@ export default function CheckOut() {
                     </div>
 
 
-                    <div className="flex justify-center my-20 items-center">
+                    <div className="flex justify-center my-5 md:my-20 items-center">
                         <span
                             onClick={handleSubmit}
                             className="btnTheme m-0">PLACE ORDER</span>
                     </div>
 
 
+                    <div className=" overflow-x-scroll md:overflow-hidden">
+                        <table className="min-w-full border-separate border-spacing-y-2 font-extrabold text-white text-center overflow-hidden">
+                            <thead>
 
+                            </thead>
+                            <tbody>
+                                <TableRow
+                                    pruductImage={"/images/CARD2.png"}
+                                    price={20.00}
+                                    quantity={15}
+                                    total={40.00}
+                                />
+                                <TableRow
+                                    pruductImage={"/images/CARD2.png"}
+                                    price={20.00}
+                                    quantity={15}
+                                    total={40.00}
+                                />
+                                <TableRow
+                                    pruductImage={"/images/CARD2.png"}
+                                    price={20.00}
+                                    quantity={15}
+                                    total={40.00}
+                                />
 
+                            </tbody>
 
+                        </table>
 
-
-
-
-                    <table className="min-w-full border-separate border-spacing-y-2 font-extrabold text-white text-center">
-                        <thead>
-
-                        </thead>
-                        <tbody>
-                            <TableRow
-                                pruductImage={"/images/CARD2.png"}
-                                price={20.00}
-                                quantity={15}
-                                total={40.00}
-                            />
-
-                        </tbody>
-
-                    </table>
-
-
+                    </div>
 
 
 
@@ -145,11 +151,9 @@ export default function CheckOut() {
 function TableRow({ pruductImage, price, total }: { pruductImage: string, price: number, quantity: number, total: number }) {
 
 
-
-
     return (
-        <tr >
-            <td className="p-4 px-32">
+        <tr>
+            <td className="p-4 ps-8  sm:px-10 md:px-20 lg:px-32 ">
                 <div className="flex items-start justify-start w-fit">
                     <CartCard
                         image={pruductImage}
@@ -159,15 +163,18 @@ function TableRow({ pruductImage, price, total }: { pruductImage: string, price:
                 </div>
             </td>
 
-            <td className="p-4 px-32">
-                <div className="flex flex-col items-start justify-start">
-                    <div>Oberon</div>
-                    <span className="m-0">Sub Total: ${price}</span>
+            <td className="p-4  sm:px-10 md:px-20 lg:px-32">
+                <div className="flex flex-col items-start justify-start text-xs sm:text-sm md:text-base lg:text-lg">
+                    <div className="font-semibold">Oberon</div>
+                    <span>${total} </span>
                 </div>
             </td>
 
-            <td className="p-4 px-32">
-                <div className="flex items-start justify-start">${total}</div>
+            <td className="p-4  sm:px-10 md:px-20 lg:px-32">
+                <div className="flex items-start justify-start text-xs sm:text-sm md:text-base lg:text-lg">
+                    <span className="m-0">Sub Total: ${price}</span>
+
+                </div>
             </td>
         </tr>
 
--- src/pages/my-cart/index.tsx
+++ src/pages/my-cart/index.tsx
@@ -3,21 +3,21 @@ import { CartCard } from "../../components/Card";
 import { useNavigate } from "react-router";
 
 export default function MyCart() {
-const navigate = useNavigate();
+    const navigate = useNavigate();
 
 
     return (
-        <div className='flex flex-col items-center justify-center gap-10'>
+        <div className='flex flex-col items-center justify-center gap-10 px-3 md-px-0'>
 
             <p className='Bold-heading'>MY CART</p>
 
-            <div className='rounded-box relative w-7xl  flex justify-center items-center cursor-pointer '>
+            <div className='rounded-box relative  w-full lg:w-6xl  flex justify-center items-center cursor-pointer '>
 
 
 
 
-                <div className="flex flex-col rounded-box-inner  gap-3  py-12  ">
-                    <table className="min-w-full border-separate border-spacing-y-2 font-extrabold text-white text-center">
+                <div className="flex flex-col rounded-box-inner gap-3 py-12 overflow-x-scroll md:overflow-hidden px-5 md:px-0">
+                    <table className="min-w-full border-separate border-spacing-y-2 font-extrabold text-white text-center overflow-hidden">
                         <thead>
                             <tr>
                                 <th className="p-4">Product</th>
@@ -33,29 +33,39 @@ const navigate = useNavigate();
                                 quantity={15}
                                 total={40.00}
                             />
-
+                            <TableRow
+                                pruductImage={"/images/CARD2.png"}
+                                price={20.00}
+                                quantity={15}
+                                total={40.00}
+                            />
+                            <TableRow
+                                pruductImage={"/images/CARD2.png"}
+                                price={20.00}
+                                quantity={15}
+                                total={40.00}
+                            />
                         </tbody>
-
                     </table>
 
 
 
 
                     {/* Sub Total */}
-                    <div className="ms-20 mt-5 w-6xl flex justify-between items-end">
+                    <div className=" md:ms-20 mt-5 lg:w-5xl flex flex-col md:flex-row justify-center  items-start md:justify-between md:items-center ">
 
                         <div
-                          onClick={()=>navigate('/shop')}
-                        className="underline hover:text-slate-500">Continue Shopping</div>
+                            onClick={() => navigate('/shop')}
+                            className="underline hover:text-slate-500">Continue Shopping</div>
 
 
 
 
-                        <div className="flex  gap-10 justify-center items-center">
+                        <div className="flex flex-col md:flex-row  gap-3 md:gap-10  items-start  justify-center md:items-center">
                             <span className=" m-0">Sub Total: $40.00</span>
                             <span
-                            onClick={()=>navigate('/checkout')}
-                            className="btnTheme m-0">GO TO CHECKOUT</span>
+                                onClick={() => navigate('/checkout')}
+                                className="btnTheme  m-0">GO TO CHECKOUT</span>
                         </div>
 
 
@@ -123,8 +133,12 @@ function TableRow({ pruductImage, price, quantity, total }: { pruductImage: stri
                 <div className="flex items-center justify-center">${total}</div>
             </td>
 
-            <td className="ps-4">
-                <img src="/images/CROSS.png" alt="" />
+            <td className="ps-4 w-28 min-w-[7rem]">
+                <img
+                    src="/images/CROSS.png"
+                    className="w-8 h-8 block z-50 object-contain"
+                    alt="cross"
+                />
             </td>
         </tr>
     )
