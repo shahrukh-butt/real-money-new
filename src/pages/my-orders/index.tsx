82f3a0d46ad9be6466abec0592bb3f5b85267554
shahrukh-butt <shahrukh.butt@salsoft.net>
2025-05-16 20:58:27 +0500

    update

--- src/pages/my-orders/index.tsx
+++ src/pages/my-orders/index.tsx
@@ -4,7 +4,7 @@ export default function MyOrders() {
 
     return (
         <div className="flex flex-col rounded-box-inner gap-0  pt-12 overflow-hidden">
-            <p className="text-2xl md:text-4xl font-extrabold px-16 mb-0">MY ORDERS</p>
+            <p className="text-2xl md:text-4xl font-extrabold px-10 md:px-16 mb-0">MY ORDERS</p>
 
             <div className="flex flex-col justify-center items-start px-6 mt-0 overflow-x-scroll">
                 <TableSection />
